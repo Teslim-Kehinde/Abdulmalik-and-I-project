bce4c0c09c37c9059be2653b8f75e184edb20315
Teslim-Kehinde <teslim.kehinde160221057@st.alsu.edu.ng>
2020-06-28 10:18:18 +0000

    I have added Abdulmalik

---
+++ builtin_func..py
@@ -0,0 +1,21 @@
+{
+     "name":"Kehinde",
+     "Age":20,
+     "Languages":["Yoruba","English","python"],
+     "Certificate":[
+                     {"name":"Motosh","place":"Ikeja"},
+                     {"name":"NimechE","place":"VI"},
+                     {"name":"Agla","place":"WA"}
+                    ]
+}
+
+
+import xml
+
+<name> Teslim </name>
+<age> 20 </age>
+<languages>  
+<Language> Yoruba </Language>
+<Langauge> English </Language>
+<Language> python </Language>
+</languages>
